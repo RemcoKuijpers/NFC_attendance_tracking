155683bb410fed7f508a71a5365a81da1bbcd316
RoboHubEindhoven <Robohubeindhoven@gmail.com>
2019-08-17 10:43:56 +0200

    Updates

--- Logger.py
+++ Logger.py
@@ -14,68 +14,75 @@ class Logger():
         self.sheets = {}
         self.day = 1
         self.sheets[self.day] = self.wb.add_sheet(self.today.strftime("%B %d, %Y"))
-        self.sheets[self.day].write(0, 0, 'ID')
-        self.sheets[self.day].write(0, 1, 'Inkloktijd')
-        self.sheets[self.day].write(0, 2, 'Uitkloktijd')
-        self.sheets[self.day].write(0, 3, 'Gewerkte tijd')
+        self.sheets[self.day].write(0, 0, 'Naam')
+        self.sheets[self.day].write(0, 1, 'Id')
+        self.sheets[self.day].write(0, 2, 'Inkloktijd')
+        self.sheets[self.day].write(0, 3, 'Uitkloktijd')
+        self.sheets[self.day].write(0, 4, 'Gewerkte tijd')
         self.inTime = {}
         self.nums = {}
         self.num = 1
         self.ids = []
 
     def clockIn(self, id):
+        name = self.db.lookUpName(id)
+
         if date.today() != self.today:
             wbr = xlrd.open_workbook('Log/test.xls')
             sheet = wbr.sheet_by_index(self.day - 1)
-            data = [sheet.cell_value(col, 0) for col in range(sheet.nrows)]
+            names = [sheet.cell_value(col, 0) for col in range(sheet.nrows)]
+            ids = [sheet.cell_value(col, 1) for col in range(sheet.nrows)]
             self.day += 1
             self.today = date.today()
             self.sheets[self.day] = self.wb.add_sheet(self.today.strftime("%B %d, %Y"))
-            self.sheets[self.day].write(0, 1, 'Inkloktijd')
-            self.sheets[self.day].write(0, 2, 'Uitkloktijd')
-            self.sheets[self.day].write(0, 3, 'Gewerkte tijd')
+            self.sheets[self.day].write(0, 2, 'Inkloktijd')
+            self.sheets[self.day].write(0, 3, 'Uitkloktijd')
+            self.sheets[self.day].write(0, 4, 'Gewerkte tijd')
 
-            for i, val in enumerate(data):
-                    self.sheets[self.day].write(i, 0, val)
-
-        name = self.db.lookUpName(id)
+            for i, val in enumerate(names):
+                self.sheets[self.day].write(i, 0, val)
+            for j, wal in enumerate(ids):
+                self.sheets[self.day].write(j, 1, wal)
 
         if name is not None:
             self.db.insert('present', id, name)
-
-            self.sheets[self.day].write(self.num, 0, id)
+            self.sheets[self.day].write(self.num, 0, name)
+            self.sheets[self.day].write(self.num, 1, id)
             self.inTime[int(id)] = datetime.now()
             self.nums[int(id)] = self.num
-            self.sheets[self.day].write(self.num, 1, self.inTime[int(id)].strftime("%d/%m/%Y %H:%M:%S"))
+            self.sheets[self.day].write(self.num, 2, self.inTime[int(id)].strftime("%d/%m/%Y %H:%M:%S"))
             self.wb.save('Log/test.xls')
             self.num += 1
 
             self.db.infoText(name + ' is succesvol ingeklokt')
 
     def clockOut(self, id):
+        name = self.db.lookUpName(id)
+
         if date.today() != self.today:
             wbr = xlrd.open_workbook('Log/test.xls')
             sheet = wbr.sheet_by_index(self.day - 1)
-            data = [sheet.cell_value(col, 0) for col in range(sheet.nrows)]
+            names = [sheet.cell_value(col, 0) for col in range(sheet.nrows)]
+            ids = [sheet.cell_value(col, 1) for col in range(sheet.nrows)]
             self.day += 1
             self.today = date.today()
             self.sheets[self.day] = self.wb.add_sheet(self.today.strftime("%B %d, %Y"))
-            self.sheets[self.day].write(0, 1, 'Inkloktijd')
-            self.sheets[self.day].write(0, 2, 'Uitkloktijd')
-            self.sheets[self.day].write(0, 3, 'Gewerkte tijd')
+            self.sheets[self.day].write(0, 2, 'Inkloktijd')
+            self.sheets[self.day].write(0, 3, 'Uitkloktijd')
+            self.sheets[self.day].write(0, 4, 'Gewerkte tijd')
         
-            for i, val in enumerate(data):
-                    self.sheets[self.day].write(i, 0, val)
-        
-        name = self.db.lookUpName(id)
+            for i, val in enumerate(names):
+                self.sheets[self.day].write(i, 0, val)
+            for j, wal in enumerate(ids):
+                self.sheets[self.day].write(j, 1, wal)
 
         if name is not None:
             self.db.deleteId('present', id)
 
             now = datetime.now()
             diff = now - self.inTime[int(id)]
-            self.sheets[self.day].write(self.nums[int(id)], 2, now.strftime("%d/%m/%Y %H:%M:%S"))
-            self.sheets[self.day].write(self.nums[int(id)], 3, str(diff))
+            self.sheets[self.day].write(self.nums[int(id)], 3, now.strftime("%d/%m/%Y %H:%M:%S"))
+            self.sheets[self.day].write(self.nums[int(id)], 4, str(diff))
             self.wb.save('Log/test.xls')
 
             self.db.infoText(name + ' is succesvol uitgeklokt')
@@ -83,19 +90,11 @@ class Logger():
 
 if __name__ == "__main__":
     Logger1 = Logger()
-    #Logger1.db.clearPersonsTable()
-    # Logger1.clockIn('4')
-    # time.sleep(5)
     Logger1.clockIn('1')
     time.sleep(5)
     Logger1.clockIn('2')
     time.sleep(3)
-    Logger1.clockOut('2')
-    time.sleep(5)
-    Logger1.clockIn('3')
-    time.sleep(4)
     Logger1.clockOut('1')
-    time.sleep(2)
-    Logger1.clockOut('3')
-    # time.sleep(2)
-    # Logger1.clockOut('4')
+    time.sleep(5)
+    Logger1.clockIn('2')
+    time.sleep(4)
--- Reader.py
+++ Reader.py
@@ -1,1 +1,7 @@
+import subprocess
+import time
 
+process = subprocess.Popen("florence")
+time.sleep(4)
+
+process.terminate()
